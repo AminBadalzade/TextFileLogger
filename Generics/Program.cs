d1a60bc0c051902b4ddac785ac53ebe937c69fd9
AminBadalzade <amin.badalzade@sa.stud.vu.lt>
2025-08-05 16:43:09 +0300

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Generics
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsAlive { get; set; }
+    }
+    public class LogEntry
+    {
+        public string Messager { get; set; }
+        public int ErrorCode { get; set; }
+
+        public DateTime TimeOfEvent { get; set; } = DateTime.UtcNow;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+
+            DemonstrateTextFileStorage();
+
+            Console.WriteLine();
+            Console.Write("Press enter to shut down...");
+            Console.ReadLine();
+        }
+        private static void DemonstrateTextFileStorage()
+        {
+            List<Person> people = new List<Person>();
+            List<LogEntry> logs = new List<LogEntry>();
+
+            //string PeopleFile = @"C:\Users\Amin0\OneDrive\İş masası\FileFor\People.csv";
+            string LogsFile = @"C:\Users\Amin0\OneDrive\İş masası\FileFor\logs.csv";
+            //File.Create(PeopleFile).Close();
+            File.Create(LogsFile).Close();
+            PopulateLists(people, logs);
+
+            //OriginalTextFileProcessor.SavePeople(people, PeopleFile);
+
+            //var newPeople = OriginalTextFileProcessor.LoadPeople(PeopleFile);
+
+            //foreach(var p in newPeople)
+            //{
+            //    Console.WriteLine($"{p.FirstName} {p.LastName} (IsAlive = {p.IsAlive})");
+            //}
+
+            OriginalTextFileProcessor.SaveLogs(logs, LogsFile);
+
+            var newLogs = OriginalTextFileProcessor.LoadLogs(LogsFile);
+            
+            foreach(var p in logs)
+            {
+                Console.WriteLine($"{p.ErrorCode} {p.Messager} at {p.TimeOfEvent.ToShortDateString()}");
+            }
+            
+        }
+
+        private static void PopulateLists(List<Person> people, List<LogEntry> logs)
+        {
+            people.Add(new Person { FirstName = "Amin", LastName = "Badalzade" });
+            people.Add(new Person { FirstName = "Emin", LastName = "Bedel", IsAlive = true});
+            people.Add(new Person { FirstName = "Edgar", LastName = "Goyjali" });
+            people.Add(new Person { FirstName = "Asif", LastName = "AsifOglu" });
+
+            logs.Add(new LogEntry {Messager = "Hello guys", ErrorCode = 9999 });
+            logs.Add(new LogEntry { Messager = "Amin is dead", ErrorCode = 444 });
+            logs.Add(new LogEntry { Messager = "Ikram is cooking", ErrorCode = 321 });
+        }
+
+    }
+}
---
+++ Generics/WithoutGenerics/OriginalTextFileProcessor.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class OriginalTextFileProcessor
+    {
+        public static List<Person> LoadPeople(string filePath)
+        {
+            List<Person> output = new List<Person>();
+
+            Person p;
+
+            var lines = File.ReadAllLines(filePath).ToList();
+
+            lines.RemoveAt(0);
+
+            foreach (var line in lines)
+            {
+                var vals = line.Split(',');
+                p = new Person();
+
+                p.FirstName = vals[0];
+                p.IsAlive = bool.Parse(vals[1]);
+                p.LastName = vals[2];
+
+                output.Add(p);
+            }
+
+            return output;
+        }
+
+        public static List<LogEntry> LoadLogs(string filePath)
+        {
+            List<LogEntry> output = new List<LogEntry>();
+
+            LogEntry log;
+
+            var lines = File.ReadAllLines(filePath).ToList();
+
+            lines.RemoveAt(0);
+
+            foreach (var line in lines)
+            {
+                var vals = line.Split(',');
+                log = new LogEntry();
+
+                log.ErrorCode = int.Parse(vals[0]);
+                log.Messager = vals[1];
+                log.TimeOfEvent = DateTime.Parse(vals[2]);
+
+                output.Add(log);
+            }
+
+            return output;
+        }
+
+        public static void SaveLogs(List<LogEntry> log, string filePath)
+        {
+            List<string> lines = new List<string>();
+
+            //Add a header row
+            lines.Add("ErrorCode,Message,TimeOfEvent");
+
+            foreach (var l in log)
+            {
+                lines.Add($"{l.ErrorCode},{l.Messager},{l.TimeOfEvent}");
+            }
+
+            File.WriteAllLines(filePath, lines);
+        }
+
+        public static void SavePeople(List<Person> people, string filePath)
+        {
+             List<string> lines = new List<string>();
+
+            //Add a header row
+            lines.Add("FirstName,IsAlive,LastName");
+
+            foreach (var p in people)
+            {
+                lines.Add($"{p.FirstName},{p.IsAlive},{p.LastName}");
+            }
+
+            File.WriteAllLines(filePath, lines);
+        }
+    }
+}
